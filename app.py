ee2efd2a9e98b81b383b80366449d990f0749ac3
kai ✨️ <129718073+kaiwashere1@users.noreply.github.com>
2024-08-26 10:20:19 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-from flask import Flask, request, jsonify, render_template
 import torch
 import torch.nn as nn
 from torchvision import models, transforms
@@ -52,4 +51,4 @@ def predict():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5001)
